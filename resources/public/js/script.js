603b879d6cd38d1a5659d52a7b7c0ff54260bfe4
binarweb <binarholdingsrl@gmail.com>
2021-09-24 19:23:23 +0300

    plaintext support for highlight.js

--- resources/public/js/script.js
+++ resources/public/js/script.js
@@ -3,5 +3,6 @@ const hljs = require('highlight.js/lib/core');
 // Highlight JS
 (function() {
     hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
+    hljs.registerLanguage('plaintext', require('highlight.js/lib/languages/plaintext'));
     hljs.highlightAll();
 })();
